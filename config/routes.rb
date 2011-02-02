c3772d513ed1938fa29334cdd11a6be5fce80675
yougovt <info@yougovt.in>
2011-02-01 16:28:17 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/home',    :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
   
   # get "pages/contact"
   # get "pages/home"
